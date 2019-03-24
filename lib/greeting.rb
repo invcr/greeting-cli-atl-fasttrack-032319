1ba6686b00b7e60b219f754f9d4df1ccee2f0944
Alex Ankirskiy <ala26@hi.is>
2019-03-24 03:03:57 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,4 +1,4 @@
 # code the #greeting method here!
-def greeting(name = "User")
-  print "Hello #{name}\t. It's nice to meet you."
+def greeting(name = "User\t")
+  print "Hello #{name}. It's nice to meet you."
 end
